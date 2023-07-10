b753a15d8f7a8619023cae4e750ce663a3822877
kj <kj@DESKTOP-8RUCSL2>
2023-07-10 16:04:25 +0900

    0710

--- chap06/src/sec01/학생예제1.java
+++ chap06/src/sec01/학생예제1.java
@@ -5,6 +5,7 @@ public class 학생예제1 {
 	public static void main(String[] args) {
 		Student s1 = new Student();
 		Student s2 = new Student();
+		Student s3 = new Student();
 		
 //		s1.age;
 //		s1.setAge(40);
